15b40d5dac2567fdd968b93d4ab8f93198eec0f5
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2016-11-29 02:18:41 +0100

    main page tablet, desktop

--- app/js/js.js
+++ app/js/js.js
@@ -2,13 +2,38 @@
  * Created by Mateusz Chybiorz on 2016-11-26.
  */
 
-
+var previousSize = window.innerWidth;
 
 $("#logo").click(function () {
-    $("#mainNav").slideToggle(200);
-    $("#geoSlaskNav").slideUp(200);
+    if(window.matchMedia('(max-width: 767px)').matches){
+        $("#mainNav").slideToggle(200);
+        $("#geoSlaskNav").slideUp(200);
+    }
 });
 
 $("#secondNav").click(function () {
-    $("#geoSlaskNav").slideToggle(150);
+    if(window.matchMedia('(max-width: 767px)').matches) {
+        $("#geoSlaskNav").slideToggle(150);
+    }
 });
+
+window.addEventListener("resize", function () {
+    changeSize();
+}, false);
+
+var pierwszaLista = document.getElementById("mainNav");
+var drugaLista = document.getElementById("geoSlaskNav");
+
+function changeSize() {
+    var currentSize = window.innerWidth;
+    if(previousSize <= 768 && currentSize > 768){
+        if(pierwszaLista.style.display){
+            $("#geoSlaskNav").slideUp(1);
+            pierwszaLista.style.removeProperty("display");
+        }
+        if(drugaLista.style.display){
+            drugaLista.style.removeProperty("display");
+        }
+    }
+    previousSize = currentSize;
+}
